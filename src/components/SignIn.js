32a60275d39a18f559b7faf58ae9d8f99b0c90fe
techStarZx <stechstar.me@gmail.com>
2024-10-30 13:48:12 +0500

    add basic layout to SignIn page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Stack from 'react-bootstrap/Stack';
+
 function SignIn() {
-  return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col xs lg="2">
-          1 of 3
-        </Col>
-        <Col md="auto">Variable width content</Col>
-        <Col xs lg="2">
-          3 of 3
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                    <Stack gap={3}>
+                        <div className="p-2">Logo Image goes here</div>
+                        <div className="p-2">Login form goes here</div>
+                    </Stack>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default SignIn;
