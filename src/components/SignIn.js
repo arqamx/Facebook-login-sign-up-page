2443931d2c693cf6206a0da96f03a1fd47678aae
techStarZx <stechstar.me@gmail.com>
2024-10-30 14:55:25 +0500

    add logo to SignIn page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,13 +4,15 @@ import Col from 'react-bootstrap/Col';
 
 import Stack from 'react-bootstrap/Stack';
 
+import Image from 'react-bootstrap/Image';
+
 function SignIn() {
     return (
         <Container>
             <Row className="justify-content-md-center">
                 <Col md="auto">
-                    <Stack gap={3}>
-                        <div className="p-2">Logo Image goes here</div>
+                    <Stack gap={5} style={{width: '396px'}}>
+                        <div className="p-2 d-flex justify-content-center align-items-center" ><Image src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" fluid className="w-50" /></div>
                         <div className="p-2">Login form goes here</div>
                     </Stack>
                 </Col>
